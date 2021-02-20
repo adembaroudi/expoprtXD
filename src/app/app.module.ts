f214d0ae3736d48531506e85ba9bec38b18b661c
Adem Baroudi <adembaroudi3177@gmail.com>
2021-02-20 16:23:22 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavComponent } from './home/nav/nav.component';
-import { BodyComponent } from './home/body/body.component';
-import { FooterComponent } from './home/footer/footer.component';
-import { HeaderComponent } from './home/header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavComponent,
-    BodyComponent,
-    FooterComponent,
-    HeaderComponent,
-    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
